6c59a326001607cfd6fd01b1d6b42a2574708671
rotsee <leo_wallentin@hotmail.com>
2023-01-01 17:23:05 +0100

    skeleton

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app>
+    <v-container>
+      <NuxtPage />
+    </v-container>
+  <v-bottom-navigation>
+    <v-btn variant="text">
+      Om Öppenhetsdatabasen
+    </v-btn>
+  </v-bottom-navigation>
+  </v-app>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-card>
+    <v-card-title>
+      Öppenhetsdatabasen
+    </v-card-title>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data: () => ({
+  }),
+  setup: async function () {
+    return {  }
+  },
+}
+</script>
+<style>
+</style>

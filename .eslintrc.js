5148157050fc310cd51d3fb5b0cff627fcdce228
rotsee <leo_wallentin@hotmail.com>
2023-01-21 16:12:17 +0100

    some VSCode mods

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,13 +4,18 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'eslint:recommended',
-    'plugin:vue/vue3-recommended',
+    "eslint:recommended",
+    "plugin:vue/vue3-recommended",
+    "@nuxtjs/eslint-config-typescript",
     "prettier",
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'comma-dangle': ['error', 'always-multiline'],
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "comma-dangle": ["error", "always-multiline"],
+    "vue/valid-v-slot": ["error", {
+      // for data-table column slots
+      "allowModifiers": true,
+    }],
   },
 }

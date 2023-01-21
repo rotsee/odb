613569871e769add0c09d82ffcb8ed98b5f3f9f7
rotsee <leo_wallentin@hotmail.com>
2023-01-21 16:27:41 +0100

    fiooter

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,20 @@
+<!-- eslint-disable vue/no-multiple-template-root -->
 <template>
-  <v-row>
+  <v-row justify-center>
     <v-col>
       <hero-chart />
+    </v-col>
+  </v-row>
+  <v-row>
+    <v-col>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras. Laoreet id donec ultrices tincidunt arcu non sodales neque. In nisl nisi scelerisque eu ultrices vitae auctor. Semper auctor neque vitae tempus quam pellentesque nec. Eros donec ac odio tempor orci dapibus. Proin fermentum leo vel orci. Facilisis sed odio morbi quis commodo. Urna et pharetra pharetra massa massa ultricies mi quis. Ac turpis egestas sed tempus urna. Libero id faucibus nisl tincidunt eget nullam non. Consequat semper viverra nam libero justo laoreet sit amet cursus. Ut enim blandit volutpat maecenas volutpat blandit aliquam etiam erat. 
+      </p>
+    </v-col>
+    <v-col>
+      <p>
+        Natoque penatibus et magnis dis parturient montes. Erat nam at lectus urna. Egestas maecenas pharetra convallis posuere morbi leo. Cras sed felis eget velit aliquet sagittis id. Sed viverra ipsum nunc aliquet bibendum enim. Donec pretium vulputate sapien nec sagittis aliquam. Amet dictum sit amet justo donec enim diam. Nibh praesent tristique magna sit amet. Urna cursus eget nunc scelerisque viverra mauris. Leo a diam sollicitudin tempor id eu. Euismod quis viverra nibh cras pulvinar mattis nunc sed blandit. At auctor urna nunc id cursus metus. Eget magna fermentum iaculis eu non diam phasellus vestibulum.
+      </p>
     </v-col>
   </v-row>
   <v-row>
@@ -12,48 +25,11 @@
 </template>
 
 <script>
-import { Bar } from "vue-chartjs"
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from "chart.js"
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
-
 export default {
-  components: { Bar },
   setup: async function () {
-    const plusMinus = await $fetch("/api/plusminus")
-    const years = Object.keys(plusMinus)
-    return { plusMinus, years }
   },
   data () {
-    const data = Object.values(this.plusMinus)
-    return {
-      chartOptions: {
-        responsive: true,
-        indexAxis: 'y',
-        
-        scales: {
-          y: {
-            stacked: true,
-          },
-          x: {
-            ticks: {
-              callback: Math.abs,
-            },
-          },
-        },
-      },
-      chartData: {
-        labels: this.years,
-        datasets: [{
-          label: "Lagändringar som minskat öppenheten",
-          data: data.map(x => -x.ökad),
-          backgroundColor: "rgba(255, 99, 132, 0.8)",
-        }, {
-          label: "Lagändringar som ökat öppenheten",
-          data: data.map(x => x.minskad),
-          backgroundColor: "rgba(75, 192, 192, 0.8)",
-        }],
-      },
-    }
+    return {}
   },
 }
 </script>

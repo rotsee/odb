b64372d1b9e2ed37052a647ef7163bdec318a9ac
rotsee <leo_wallentin@hotmail.com>
2023-01-22 23:02:55 +0100

    col

--- pages/index.vue
+++ pages/index.vue
@@ -6,12 +6,12 @@
     </v-col>
   </v-row>
   <v-row>
-    <v-col>
+    <v-col cols="auto" lg="6">
       <p>
         De flesta av de hundratalet lagändringarna i Offentlighets- och sekretesslagen (OSL) under den här mandatperioden är var för sig – med några undantag – oftast ganska små. Få av dem förändrar ensamma insynen i svenska myndigheters arbete så mycket att det förtjänar några rubriker. Men om var och en av dem inskränker öppenheten bara något, så riskerar de att sammantaget driva samhället mot ökad slutenhet.
       </p>
     </v-col>
-    <v-col>
+    <v-col cols="auto" lg="6">
       <p>
         För att få en bild av hela lagändringspusslet, har vi byggt en databas över lagändringar i OSL, med start i början av förra manadatperioden. 
         Databasen innehåller omkring 200 lagändringar i OSL (alla substantiella ändringar av lagtexter, men inte vissa rubrikändringar, korrektur och liknande).

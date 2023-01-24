547daef29517539cb366e58bbadd9efb7edb4551
rotsee <leo_wallentin@hotmail.com>
2023-01-24 18:37:40 +0100

    use setup

--- components/HeroChart.vue
+++ components/HeroChart.vue
@@ -1,11 +1,55 @@
+<script setup>
+
+import { Bar } from "vue-chartjs"
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from "chart.js"
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+const { pending, data: rawData } = await useLazyFetch("/api/plusminus", { method: "POST" })
+
+const chartData = computed(() => {
+  if (pending.value) {
+    return null
+  }
+  const years = Object.keys(rawData.value)
+  const data = Object.values(rawData.value)
+  return {
+    labels: years,
+    datasets: [{
+      label: "Lagändringar som minskat öppenheten",
+      data: data.map(x => -x.ökad),
+      backgroundColor: "rgba(255, 99, 132, 0.8)",
+    }, {
+      label: "Lagändringar som ökat öppenheten",
+      data: data.map(x => x.minskad),
+      backgroundColor: "rgba(75, 192, 192, 0.8)",
+    }],
+  }
+})
+
+const chartOptions = {
+  responsive: true,
+  indexAxis: 'y',
+  scales: {
+    y: {
+      stacked: true,
+    },
+    x: {
+      ticks: {
+        callback: Math.abs,
+      },
+    },
+  },
+}
+
+</script>
 <template>
-  <v-card max-width="700">
+  <v-card max-width="700" :loading="pending">
     <v-card-title>
       Så har de senaste årens lagändringar förändrat öppenheten i samhället
     </v-card-title>
     <v-card-text>
-      <Bar
-        id="hero-chart"
+      <bar
+        v-if="chartData"
         :options="chartOptions"
         :data="chartData"
       />
@@ -13,52 +57,5 @@
     </v-card-text>
   </v-card>
 </template>
-
-<script>
-import { Bar } from "vue-chartjs"
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from "chart.js"
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
-
-export default {
-  components: { Bar },
-  setup: async function () {
-    const plusMinus = await $fetch("/api/plusminus")
-    const years = Object.keys(plusMinus)
-    return { plusMinus, years }
-  },
-  data () {
-    const data = Object.values(this.plusMinus)
-    return {
-      chartOptions: {
-        responsive: true,
-        indexAxis: 'y',
-        
-        scales: {
-          y: {
-            stacked: true,
-          },
-          x: {
-            ticks: {
-              callback: Math.abs,
-            },
-          },
-        },
-      },
-      chartData: {
-        labels: this.years,
-        datasets: [{
-          label: "Lagändringar som minskat öppenheten",
-          data: data.map(x => -x.ökad),
-          backgroundColor: "rgba(255, 99, 132, 0.8)",
-        }, {
-          label: "Lagändringar som ökat öppenheten",
-          data: data.map(x => x.minskad),
-          backgroundColor: "rgba(75, 192, 192, 0.8)",
-        }],
-      },
-    }
-  },
-}
-</script>
 <style>
 </style>

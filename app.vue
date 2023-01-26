96f36c78b1065f366b67f72bfe62a47190131751
rotsee <leo_wallentin@hotmail.com>
2023-01-26 17:18:55 +0100

    more resonsive app-bar

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar app absolute class="py-2 px-4">
+    <v-app-bar app absolute class="pr-4 pt-2">
       <v-spacer  class="hidden-md-and-down"/>
       <v-app-bar-title class="app_bar_title">
         <nuxt-link href="/">
@@ -10,10 +10,11 @@
           >Öppenhetsdatabasen</component>
         </nuxt-link>
       </v-app-bar-title>
-      <a href="https://www.sjf.se"  class="hidden-md-and-down">
+      <a href="https://www.sjf.se">
         <nuxt-img
+          id="sjf-logo"
           max-width="222"
-          height="60"
+          max-height="60"
           src="/img/sjf-logo.svg"
           alt="Journalistförbundet"
         />
@@ -37,5 +38,29 @@
 <style>
 .app_bar_title a {
   text-decoration: none;
+}
+.v-container {
+  max-width: 1200px;
+}
+#sjf-logo {
+  height: 60px;
+}
+@media only screen 
+and (max-device-width: 800px) {
+  #sjf-logo {
+    height: 40px;
+  }
+}
+@media only screen 
+and (max-device-width: 600px) {
+  #sjf-logo {
+    height: 30px;
+  }
+}
+@media only screen 
+and (max-device-width: 400px) {
+  #sjf-logo {
+    height: 20px;
+  }
 }
 </style>

31d2c92f258e17a1a679d05bff2392c7d417d495
rotsee <leo_wallentin@hotmail.com>
2023-05-10 21:05:20 +0200

    typsnitt, topp

--- components/HeroChart.vue
+++ components/HeroChart.vue
@@ -4,6 +4,7 @@ import { ref } from "vue"
 import { Bar } from "vue-chartjs"
 import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from "chart.js"
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+ChartJS.defaults.font.family = "Futura Book"
 
 
 const { pending, data: rawData } = await useFetch("/api/plusminus")

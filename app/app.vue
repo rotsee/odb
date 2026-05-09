baa9e5ceccd199c1dd4d0fbf2b7fe95fc17e6a1a
rotsee <leo_wallentin@hotmail.com>
2026-05-09 11:08:52 +0200

    nuxt 4

---
+++ app/app.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <v-toolbar flat color="#e7e7e7">
+      <template #title>
+        <nuxt-link href="/" class="app_bar_title">
+          <h1>Öppenhetsdatabasen</h1>
+        </nuxt-link>
+      </template>
+    </v-toolbar>
+
+    <v-main>
+      <NuxtPage keep-alive />
+    </v-main>
+
+    <v-footer>
+      <small>
+        Öppenhetsdatabasen drivs av 
+        <a href="https://www.sjf.se/yrkesfragor/yttrandefrihet/yttrandefrihetsgruppen">Journalistförbundets yttrandefrihetsgrupp</a>. 
+      </small>
+    </v-footer>
+  </v-app>
+</template>
+<style>
+a.app_bar_title {
+  text-decoration: none;
+}
+.v-container {
+  max-width: 1200px;
+}
+</style>
